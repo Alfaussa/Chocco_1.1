3f23ed288b21f27ff787eacec2d08fd68322fcf7
Alfiya <al.yausheva.com>
2022-02-28 17:44:55 +0100

    12

--- main.js
+++ main.js
@@ -70,7 +70,7 @@ const init = () => {
    myCollection = new ymaps.GeoObjectCollection({}, {
      draggable: false,
      iconLayout: 'default#image',
-     iconImageHref: '/images/marker.svg',
+     iconImageHref: 'images/marker.svg',
      iconImageSize: [46, 57],
      iconImageOffset: [-35, -52]
    });
